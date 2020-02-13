2fc825e267897b31e5017eefc8c58756d6a4306c
daksh <daksh3451@gmail.com>
2020-02-13 18:51:52 -0500

    added a mouse listener to menu

--- src/Menu.java
+++ src/Menu.java
@@ -1,16 +1,58 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
 
-public class Menu extends JPanel {
-
+public class Menu extends JPanel implements MouseListener {
     ArrayList<Student> students = new ArrayList<Student>();
+    ArrayList<Table> tables = new ArrayList<Table>();
 
+    public Menu(){
+        this.addMouseListener(this);
+    }
 
-    private void addStudent(){
+    private void addStudent(Student student){
+        this.students.add(student);
+    }
+
+    private void removeStudent(Student student){
+        this.students.remove(student);
+    }
+
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        setDoubleBuffered(true);
+
+        //the greatest gui goes here
+        g.fillRect(50,50,50,50);
+
+        repaint();
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.println(e.getPoint().toString());
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
 
     }
 
-    private void removeStudent(Studnt student){
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
 
     }
 }
