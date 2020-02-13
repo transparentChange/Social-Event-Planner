2482000c0cd6d16c0596cc2591e7cb9239311741
daksh <daksh3451@gmail.com>
2020-02-13 11:25:46 -0500

    Initial commit

---
+++ src/Menu.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class Menu extends JPanel {
+
+    ArrayList<Student> students = new ArrayList<Student>();
+
+
+    private void addStudent(){
+
+    }
+
+    private void removeStudent(Studnt student){
+
+    }
+}
