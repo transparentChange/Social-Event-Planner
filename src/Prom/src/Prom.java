12994e6f5c3ed1bc5fe2c211de104ab3bc02ff05
transparentPresence <50775016+transparentPresence@users.noreply.github.com>
2020-02-14 16:42:13 -0500

    Create Prom.java

---
+++ src/Prom/src/Prom.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+
+public class Prom extends JFrame {
+	static private TicketingSystem ticketingPanel;
+	private ArrayList<Student> students;
+	private ArrayList<Table> tables;
+	// private FloorPlanSystem floorPlanPanel;
+	//asdf
+	public Prom() {
+		super("Prom");
+        
+		this.students = new ArrayList<Student>();
+        this.tables = new ArrayList<Table>();
+        this.ticketingPanel = new TicketingSystem(students, tables);
+        
+        // this.add(floorPlanPanel);
+        this.add(ticketingPanel);
+        
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(800, 600);
+        this.requestFocusInWindow();
+        this.setVisible(true);
+	}
+	
+	public static void main(String[] args){
+        
+    }
+
+    void redraw(){
+        
+    }
+}
