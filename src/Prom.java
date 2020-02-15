44fd3db27328c8f7dc6e18ee777be13f4751eedd
transparentPresence <50775016+transparentPresence@users.noreply.github.com>
2020-02-14 19:27:02 -0500

    Update Prom.java

--- src/Prom.java
+++ src/Prom.java
@@ -11,16 +11,11 @@ import javax.swing.JTextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Prom extends JFrame implements ActionListener {
+public class Prom extends JFrame {
 	static private TicketingSystem ticketingPanel;
 	private ArrayList<Student> students;
 	private ArrayList<Table> tables;
-    
-	private JTextField idField;
-	private JTextField passwordField;
-	private StudentLoginButton studentButton;
-	private JButton loginButton;
-	private JButton createAccountButton;
+	private LoginPanel loginPanel;
 	
   // constants that hold game window size information
   private final static int WINDOW_WIDTH = 1366;
@@ -36,30 +31,12 @@ public class Prom extends JFrame implements ActionListener {
         this.setPreferredSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
         this.setSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
         
-		JPanel loginPanel = new JPanel(new GridBagLayout());
+		loginPanel = new LoginPanel(WINDOW_HEIGHT, WINDOW_WIDTH);
 		
-		loginPanel.setFocusable(false);
-		loginPanel.setBounds(getWidth() / 10, 0, getWidth() / 5, WINDOW_HEIGHT);
-		loginPanel.setOpaque(false);
-		
-		studentButton = new StudentLoginButton(0, 0);
-		
-		loginButton = new JButton("Login");
-		loginButton.addActionListener(this);
-		
-		createAccountButton = new JButton("Don't have an account? Click to create a new account");
-		
-		idField = new JTextField(20);
-		passwordField = new JTextField(20);
-		
-		loginPanel.add(studentButton);
-		loginPanel.add(idField);
-		loginPanel.add(passwordField);
-		loginPanel.add(loginButton);
+		//loginButton.addActionListener(this);
 		
 		this.students = new ArrayList<Student>();
         this.tables = new ArrayList<Table>();
-        
         
         // this.add(floorPlanPanel);
         //this.add(ticketingPanel);
@@ -69,18 +46,23 @@ public class Prom extends JFrame implements ActionListener {
         this.setVisible(true);
 	}
 	
-	public void actionPerformed(ActionEvent evt) {
-		Object source = evt.getSource();
-		if (source == loginButton) {
-			this.getContentPane().removeAll();
-			this.setContentPane(new JPanel(new BorderLayout()));
-			
-			ticketingPanel = new TicketingSystem(students, tables);
-			this.add(ticketingPanel);
-			
-			revalidate();
-			repaint();
-		}
+	static public int getWindowHeight() {
+		return WINDOW_HEIGHT;
+	}
+	
+	static public int getWindowWidth() {
+		return WINDOW_WIDTH;
+	}
+	
+	private void toTicketingSystem() {
+		this.getContentPane().removeAll();
+		this.setContentPane(new JPanel(new BorderLayout()));
+		
+		ticketingPanel = new TicketingSystem(students, tables);
+		this.add(ticketingPanel);
+		
+		revalidate();
+		repaint();
 	}
 	
 	public static void main(String[] args){
@@ -89,5 +71,41 @@ public class Prom extends JFrame implements ActionListener {
 
     void redraw(){
         
+    }
+    
+    public class LoginPanel extends JPanel implements ActionListener {
+    	private StudentLoginButton studentButton;
+    	private JTextField idField;
+    	private JTextField passwordField;
+    	private JButton loginButton;
+    	private JButton createAccountButton;
+    	
+    	LoginPanel(int windowHeight, int windowWidth) {
+    		this.setFocusable(false);
+    		this.setBounds(windowWidth / 10, 0, windowWidth / 5, windowHeight);
+    		this.setOpaque(false);
+    		this.setLayout(new GridBagLayout());
+    		
+    		studentButton = new StudentLoginButton(0, 0);
+    		createAccountButton = new JButton("Don't have an account? Click to create a new account");
+    		
+    		idField = new JTextField(20);
+    		passwordField = new JTextField(20);
+    		
+    		loginButton = new JButton("Login");
+    		loginButton.addActionListener(this);
+    			
+    		this.add(studentButton);
+    		this.add(idField);
+    		this.add(passwordField);
+    		this.add(loginButton);
+    	}
+    	
+    	public void actionPerformed(ActionEvent evt) {
+    		Object source = evt.getSource();
+    		if (source == loginButton) {
+    			toTicketingSystem();
+    		}
+    	}
     }
 }
