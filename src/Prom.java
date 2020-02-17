8f237d9fe0f2226f2d7a44968e11f6ea0c66cc4b
daksh <daksh3451@gmail.com>
2020-02-16 19:14:39 -0500

    cleaned up imports and comments Still sorry about screwing up the layout! tried fixing it but have no idea

--- src/Prom.java
+++ src/Prom.java
@@ -1,19 +1,7 @@
-import java.awt.BorderLayout;
-import java.awt.GridBagLayout;
 import java.awt.Dimension;
 import java.util.ArrayList;
-
-import javax.imageio.ImageIO;
-import javax.swing.JPanel;
 import javax.swing.JFrame;
-import javax.swing.JButton;
-import javax.swing.JTextField;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.util.Scanner;
 
 public class Prom extends JFrame {
     private TicketingSystem ticketingPanel;
@@ -28,34 +16,23 @@ public class Prom extends JFrame {
 
     public Prom() {
         super("Prom");
-        
-        BufferedImage image = null;
-        try {
-          image = ImageIO.read(new File("src/loginBackground.jpg"));
-        } catch (Exception e) {
-          e.printStackTrace();
-        }
-        //this.setContentPane(new BackImage(WINDOW_WIDTH, WINDOW_HEIGHT, image));
-        
-        //this.setResizable(false);
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setPreferredSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
         this.setSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
-        //this.setLayout(new GridBagLayout());
-        //loginButton.addActionListener(this);
 
         this.students = new ArrayList<Student>();
         this.tables = new ArrayList<Table>();
 
         this.ticketingPanel = new TicketingSystem(students,tables);
 
-        this.add(ticketingPanel, BorderLayout.CENTER);
+        this.add(ticketingPanel);
 
         this.requestFocusInWindow();
         this.setVisible(true);
     }
 
     public static void main(String[] args){
-        Prom prom = new Prom();
+        new Prom();
     }
 }
