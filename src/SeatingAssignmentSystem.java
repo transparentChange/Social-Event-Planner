bbfa3b38f71b3e62ab043f5a63dfd09d43c93a84
daksh <daksh3451@gmail.com>
2020-02-19 23:40:16 -0500

    added a temp SeatingAssignmentSystem 2.0 because im autistic and forgot to add to git

---
+++ src/SeatingAssignmentSystem.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class SeatingAssignmentSystem {
+    static ArrayList<Table> assignTables(ArrayList<Student> s, int maxTables, int tableSize){
+        return new ArrayList<Table>();
+    }
+}
